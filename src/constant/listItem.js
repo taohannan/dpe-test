7a703e1aa5a35ba68a24505430b374c93d8bb9f8
taohannan <taohannan@gmail.com>
2020-12-27 21:31:12 +0800

    implement dragaable and dropable

--- src/constant/listItem.js
+++ src/constant/listItem.js
@@ -1,102 +1,99 @@
-export  const ListItem =[{
-    //column 1
-},
-    {   //column 2
-        'col1-header': 'Lorem ipsum dolor sit amet',
-        'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col2-header': 'Lorem ipsum dolor sit amet',
-        'col2-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col3-header': 'Lorem ipsum dolor sit amet',
-        'col3-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col4-header': 'Lorem ipsum dolor sit amet',
-        'col4-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col5-header': 'Lorem ipsum dolor sit amet',
-        'col5-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col6-header': 'Lorem ipsum dolor sit amet',
-        'col6-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+export  const ListItem ={
+    items :[{
+
+        //column 1
+        'col1-header': '',
+        'col1-desc': '',
+        'id' : '1',
+
     },
-    { //column 3
-         },
-    {
-        'col1-header': 'Lorem ipsum dolor sit amet',
-        'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col2-header': 'Lorem ipsum dolor sit amet',
-        'col2-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col3-header': 'Lorem ipsum dolor sit amet',
-        'col3-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col4-header': 'Lorem ipsum dolor sit amet',
-        'col4-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col5-header': 'Lorem ipsum dolor sit amet',
-        'col5-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col6-header': 'Lorem ipsum dolor sit amet',
-        'col6-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-    },
-    {
-        //column 5
-    },
-    {
-        //column 6
-    },
-    {
-        'col1-header': 'Lorem ipsum dolor sit amet',
-        'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col2-header': 'Lorem ipsum dolor sit amet',
-        'col2-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col3-header': 'Lorem ipsum dolor sit amet',
-        'col3-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col4-header': 'Lorem ipsum dolor sit amet',
-        'col4-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col5-header': 'Lorem ipsum dolor sit amet',
-        'col5-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col6-header': 'Lorem ipsum dolor sit amet',
-        'col6-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-    },
-    {
-        //column 8
+        {   //column 2
+            'col1-header': 'Lorem ipsum dolor sit amet',
+            'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+            'id' : '2',
+
+        },
+        { //column 3
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '3',
+        },
+        {
+            'col1-header': 'Lorem ipsum dolor sit amet',
+            'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+            'id' : '4',
+        },
+        {
+            //column 5
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '5',
+        },
+        {
+            //column 6
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '6',
+        },
+        {
+            'col1-header': 'Lorem ipsum dolor sit amet',
+            'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+            'id' : '7',
+        },
+        {
+            //column 8
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '8',
+        }
+        ,{
+            'col1-header': 'Lorem ipsum dolor sit amet',
+            'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+            'id' : '9',
+        },
+        {
+            //column 10
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '10',
+        },
+        {
+            //column 11
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '11',
+        },
+        {
+            //column 12
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '12',
+        },
+        {
+            //column 13
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '13',
+        },
+        {
+            //column 14
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '14',
+        },
+        {
+            //column 15
+            'col1-header': '',
+            'col1-desc': '',
+            'id' : '15',
+        },
+        {
+            'col1-header': 'Lorem ipsum dolor sit amet',
+            'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
+            'id' : '16',
+
+        }],
+    columns :{
+        id : 'column-id'
     }
-    ,{
-        'col1-header': 'Lorem ipsum dolor sit amet',
-        'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col2-header': 'Lorem ipsum dolor sit amet',
-        'col2-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col3-header': 'Lorem ipsum dolor sit amet',
-        'col3-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col4-header': 'Lorem ipsum dolor sit amet',
-        'col4-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col5-header': 'Lorem ipsum dolor sit amet',
-        'col5-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col6-header': 'Lorem ipsum dolor sit amet',
-        'col6-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-    },
-    {
-        //column 10
-    },
-    {
-        //column 11
-    },
-    {
-        //column 12
-    },
-    {
-        //column 13
-    },
-    {
-        //column 14
-    },
-    {
-        //column 15
-    },
-    {
-        'col1-header': 'Lorem ipsum dolor sit amet',
-        'col1-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col2-header': 'Lorem ipsum dolor sit amet',
-        'col2-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col3-header': 'Lorem ipsum dolor sit amet',
-        'col3-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col4-header': 'Lorem ipsum dolor sit amet',
-        'col4-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col5-header': 'Lorem ipsum dolor sit amet',
-        'col5-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-        'col6-header': 'Lorem ipsum dolor sit amet',
-        'col6-desc': 'In eget ligula ut est interdum finibus. Etiam consectetur, libero tincidunt vulputate fermentum, nisi nulla cursus turpis.',
-    }];
+};
